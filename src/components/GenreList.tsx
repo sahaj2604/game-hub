e2ca1a418b78364da36a0b74b49981653ef8d76e
sahaj singh <sahajabhin@gmail.com>
2025-07-25 21:22:05 +0530

    Showing a spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,12 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImage from "../services/image-url";
 
 const GenreList = () => {
-  const { error, data } = useGenres();
+  const { error, data,isLoading } = useGenres();
   return (
+    <>
+    {isLoading && <Spinner/>}
     <List>
       {error && <p>{error}</p>}
       {data.map((genre) => (
@@ -21,6 +23,7 @@ const GenreList = () => {
         </ListItem>
       ))}
     </List>
+    </>
   );
 };
 

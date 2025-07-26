c0e3d5d2d67a2ba047e121240a1b4f53ecd066a7
sahaj singh <sahajabhin@gmail.com>
2025-07-26 09:39:04 +0530

    Building platform selector

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError, type AxiosRequestConfig } from "axios";
-import type { Genre } from "./useGenres";
-
 interface FetchResponse<T>{
     count:number;
     results:T[]
 }
-const useData = <T>(endpoint:string,requestConfig?:AxiosRequestConfig,deps:any[]) => {
+const useData = <T>(endpoint:string,requestConfig?:AxiosRequestConfig,deps?:any[]) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
